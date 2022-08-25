b3cdd181161a3fee5f8a9fc35e2bcba173606939
gabriels <boskowski96@gmail.com>
2022-08-25 17:34:36 +0200

    53 page, bootstrap

--- src/main/java/com/example/controller/InfoController.java
+++ src/main/java/com/example/controller/InfoController.java
@@ -22,7 +22,7 @@ public class InfoController {
 
     @GetMapping("/info/prop")
     boolean myProp(){
-        return myProp.isAllowMultipleTasksFromTemplate();
+        return myProp.getTemplate().isAllowMultipleTasks();
     }
 }
 
